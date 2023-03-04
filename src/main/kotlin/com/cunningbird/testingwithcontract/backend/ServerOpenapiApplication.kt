fb486aa4cf7cc1e1bf642e1806ba1b059624edf0
CunningBird <68229355+CunningBird@users.noreply.github.com>
2023-03-04 15:52:06 +0300

    initial commit

---
+++ src/main/kotlin/com/cunningbird/testingwithcontract/backend/ServerOpenapiApplication.kt
@@ -0,0 +1,11 @@
+package com.cunningbird.testingwithcontract.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerOpenapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerOpenapiApplication>(*args)
+}
